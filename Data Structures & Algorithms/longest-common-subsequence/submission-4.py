db093508cee1a9d719af0972621b48c359a78541
Yaoxi Li <89698781+Whiteblank0@users.noreply.github.com>
2026-07-17 15:19:44 +0800

    Add: longest-common-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-4.py
@@ -0,0 +1,19 @@
+import functools
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n = len(text1)
+        m = len(text2)
+
+        f = [0] * (m+1)
+        for i, x in enumerate(text1):
+            pre = f[0]
+            for j, y in enumerate(text2):
+                temp = f[j+1]
+                if x == y:
+                    f[j+1] = pre + 1
+                else:
+                    f[j+1] = max(f[j], f[(j+1)])
+                pre = temp
+        
+        return f[m]
