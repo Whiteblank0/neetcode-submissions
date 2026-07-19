03b79be3a67743d0f0411039b2ce0906ba8b5e51
Yaoxi Li <89698781+Whiteblank0@users.noreply.github.com>
2026-07-19 15:58:58 +0800

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.py
@@ -0,0 +1,16 @@
+from functools import cache
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+
+        @cache
+        def dfs(i, hold):
+            if i < 0:
+                return 0 if not hold else float('-inf')
+            if hold:
+                return max(dfs(i-1, True), dfs(i-1, False) - prices[i])
+            else:
+                return max(dfs(i-1, False), dfs(i-1, True) + prices[i])
+            
+        return dfs(n-1, False)
