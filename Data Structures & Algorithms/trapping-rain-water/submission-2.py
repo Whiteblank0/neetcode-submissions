2e425cf1913b39c4357dd54c213d71d6cbc604e2
Yaoxi Li <89698781+Whiteblank0@users.noreply.github.com>
2026-07-25 21:35:12 +0800

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        ans = 0
+        st = []
+
+        for i, h in enumerate(height):
+            while st and h >= height[st[-1]]:
+                bottom_h = height[st.pop()]
+                if not st:
+                    break
+                left = st[-1]
+                dh = min(height[left], h) - bottom_h
+                ans += dh * (i - left - 1)
+            st.append(i)
+        
+        return ans
