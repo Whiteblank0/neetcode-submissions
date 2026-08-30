908c7e562d0daa68fb7453f82210a1990c8402bd
Yaoxi Li <89698781+Whiteblank0@users.noreply.github.com>
2026-08-30 15:19:48 +0800

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,13 @@
+from functools import cache
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        
+        for i in range(n - 1, -1, -1):
+            for a in range(1, amount + 1):
+                dp[a] += dp[a - coins[i]] if a >= coins[i] else 0
+
+        return dp[amount]
